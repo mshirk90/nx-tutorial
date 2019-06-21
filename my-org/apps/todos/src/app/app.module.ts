b6d2d613af8684cc4a223b2fbcdc81975554d964
mshirk90 <mshirk90@gmail.com>
2019-06-20 21:47:26 -0500

    initial commit

---
+++ my-org/apps/todos/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component';
+import { UiModule } from '@my-org/ui';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    UiModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ my-org/libs/ui/src/lib/todos/todos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from '@my-org/data';
+
+@Component({
+  selector: 'my-org-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+@Input() todos: Todo[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
